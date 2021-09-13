2542de85cb8f6f902c2ef892d89b89270a1e9fde
ken-suyama <kh_6bbl@i.softbank.jp>
2021-09-13 21:31:54 +0900

    Revert "photo library"
    This reverts commit 5a289dba343cda4b909fab8094eb2b66d870104b.

--- study_swift/TabBarController.swift
+++ study_swift/TabBarController.swift
@@ -10,13 +10,12 @@ class TabBarController: UITabBarController, UITabBarControllerDelegate {
     private func setupVc() {
         let storyboard = UIStoryboard(name: "TimeLine", bundle: nil)
         let viewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "TimeLine")
-        let storyboard2 = UIStoryboard(name: "PhotoLibrary", bundle: nil)
-        let viewController2: UIViewController = storyboard2.instantiateViewController(withIdentifier: "PhotoLibrary")
+        let viewController2: UIViewController = UIViewController()
+//        viewController.inject(with: dependency)
+//        let tabBarController = UITabBarController()
         let tabOneBarItem = UITabBarItem(title: "TL", image: UIImage(systemName: "house"), selectedImage: UIImage(named: "selectedImage.png"))
-        let tabTwoBarItem = UITabBarItem(title: "PL", image: UIImage(systemName: "photo"), selectedImage: UIImage(named: "selectedImage.png"))
         
         viewController.tabBarItem = tabOneBarItem
-        viewController2.tabBarItem = tabTwoBarItem
         self.viewControllers = [viewController, viewController2]
     }
 }
